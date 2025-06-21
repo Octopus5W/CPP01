74c5d12432c72233fddf96f0737bd4c077461c45
OctopusSW <hugo.dlbcq@gmail.com>
2025-06-21 18:08:25 +0200

    ex00 move constructor

--- ex00/Zombie.cpp
+++ ex00/Zombie.cpp
@@ -6,11 +6,13 @@
 /*   By: hdelbecq <hdelbecq@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 20:54:38 by hdelbecq          #+#    #+#             */
-/*   Updated: 2025/05/28 21:25:57 by hdelbecq         ###   ########.fr       */
+/*   Updated: 2025/06/21 18:08:02 by hdelbecq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
+
+Zombie::Zombie(std::string name) : _name(name){};
 
 Zombie::~Zombie()
 {
@@ -21,5 +23,3 @@ void Zombie::announce()
 {
 	std::cout << _name << ": BiaiiiiinzzzZ...." << std::endl;
 }
-
-Zombie::Zombie(std::string name) : _name(name){};
